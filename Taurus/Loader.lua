ecafd395cf17e570a9208b246a502869251d85f7
GhostDuckyy <95024755+GhostDuckyy@users.noreply.github.com>
2022-09-04 23:06:50 +0800

    Update Loader.lua

--- Taurus/Loader.lua
+++ Taurus/Loader.lua
@@ -1,1 +1,17 @@
+local request = (syn and syn.request) or (http and http.request) or request or http_request or nil
+getgenv().source = nil
 
+while task.wait() do
+    if request ~= nil then
+        getgenv().source = request({Url = "https://raw.githubusercontent.com/GhostDuckyy/Taurus/main/Loader.lua",Method = "GET"})
+        if getgenv().source.Success == true then break; end
+    else
+        break;
+    end
+end
+
+if getgenv().source ~= nil and getgenv().source.Body then
+    loadstring(getgenv().source.Body)()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostDuckyy/Taurus/main/Loader.lua",true))()
+end
